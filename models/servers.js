748bdd0f1f503601f825667a35ca3720c354e918
Maxpain177 <Maxpain177@gmail.com>
2014-02-24 21:59:29 +0700

    improved server start scripts

--- models/servers.js
+++ models/servers.js
@@ -14,16 +14,17 @@ Meteor.startup(function() {
 			if (!(matchId && serverName && map && type && team1_id && team2_id)) throw new Meteor.Error('Нет аргументов');
 			if (!Servers.findOne({name: serverName})) throw new Meteor.Error('Сервер не найден');
 			var server = Servers.findOne({name: serverName}),
-				team1Name = Teams.find(team1_id).name,
-				team2Name = Teams.find(team2_id).name,
+				team1Name = Teams.findOne(team1_id).name,
+				team2Name = Teams.findOne(team2_id).name,
 				maxPlayers = parseInt(type[0], 10) + parseInt(type[2], 10) + 1 ,
-				port = server.lastUsedPort + 1
+				port = server.lastUsedPort + 1 ,
+				password = var randomstring = Math.random().toString(36).slice(-8);
 			c.exec('cd ~/hlds/cw && screen -AdmS comeback.cw-' + matchId + ' ./hlds_run -game cstrike -port ' + port + ' +maxplayers ' + maxPlayers + ' +map ' + map + ' -pingboost 3 -master -secure', function() {
 				var rconC = new RCON(server.ip, port, server.password);
 				rconC.query('cw_start ' + team1Name + ' ' + team2Name);
 			});
 			Servers.update({name: serverName}, {$set: {lastUsedPort: port}}, function() {
-				Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port}});
+				Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port, password: password}});
 			});
 		};
 	}
