a59cfccdf819723180f034c50e029e26f084b23a
Maxpain177 <Maxpain177@gmail.com>
2014-02-24 21:46:34 +0700

    1

--- server/methods/match.js
+++ server/methods/match.js
@@ -64,7 +64,7 @@ Meteor.methods({
 			}
 		}, function() {
 			var match = Matches.findOne(matchId);
-			Servers.start(match.id, match.server, match.map, match.type, match.team1._id, match.team2._id);
+			Servers.start(match._id, match.server, match.map, match.type, match.team1._id, match.team2._id);
 		});
 	}
 });
