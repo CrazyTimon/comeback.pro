53e9beff4c5f47192953e2894615aab11965d3af
Maxpain177 <Maxpain177@gmail.com>
2014-03-05 23:25:09 +0700

    0.7-rc0

--- models/servers.js
+++ models/servers.js
@@ -17,35 +17,36 @@ Meteor.startup(function() {
 				return;
 			});
 
-			Servers[server.name].start = function(matchId, serverName, game, map, type, team1_id, team2_id) {
-				if (!(matchId && serverName && map && type && team1_id && team2_id)) throw new Meteor.Error('Нет аргументов');
-				if (!Servers.findOne({name: serverName})) throw new Meteor.Error('Сервер не найден');
-				var server = Servers.findOne({name: serverName}),
-					team1Name = Teams.findOne(team1_id).name,
-					team2Name = Teams.findOne(team2_id).name,
-					maxPlayers = parseInt(type[0], 10) + parseInt(type[2], 10) + 1 ,
-					port = server.lastUsedPort + 1 ,
-					password = randomstring = Math.random().toString(36).slice(-8),
-					path = server[game].config.path;
-				switch (game) {
-					case 'cs16': {
-						Servers[server.name].sshConnection.exec('cd ' + path + ' && screen -AdmS comeback.cw-' + matchId + ' ./hlds_run -game cstrike -port ' + port + ' +maxplayers ' + maxPlayers + ' +map ' + map + ' sv_password ' + password + ' -pingboost 3 -master -secure', function() {
-							var rconConnection = new RCON(server.ip, port, server.password);
-							rconConnection.query('cw_start ' + team1Name + ' ' + team2Name + ' ' + matchId);
-						});
+			Servers.[server.name].sshConnection.on('ready', function() {
+				Servers[server.name].start = function(matchId, serverName, game, map, type, team1_id, team2_id) {
+					if (!(matchId && serverName && map && type && team1_id && team2_id)) throw new Meteor.Error('Нет аргументов');
+					if (!Servers.findOne({name: serverName})) throw new Meteor.Error('Сервер не найден');
+					var server = Servers.findOne({name: serverName}),
+						team1Name = Teams.findOne(team1_id).name,
+						team2Name = Teams.findOne(team2_id).name,
+						maxPlayers = parseInt(type[0], 10) + parseInt(type[2], 10) + 1 ,
+						port = server.lastUsedPort + 1 ,
+						password = randomstring = Math.random().toString(36).slice(-8),
+						path = server[game].config.path;
+					switch (game) {
+						case 'cs16': {
+							Servers[server.name].sshConnection.exec('cd ' + path + ' && screen -AdmS comeback.cw-' + matchId + ' ./hlds_run -game cstrike -port ' + port + ' +maxplayers ' + maxPlayers + ' +map ' + map + ' sv_password ' + password + ' -pingboost 3 -master -secure', function() {
+								var rconConnection = new RCON(server.ip, port, server.password);
+								rconConnection.query('cw_start ' + team1Name + ' ' + team2Name + ' ' + matchId);
+							});
+						}
+						case 'csgo': {
+							// Future...
+						}
+						case 'css': {
+							// Future...
+						}
 					}
-					case 'csgo': {
-						// Future...
-					}
-					case 'css': {
-						// Future...
-					}
-				}
-
-				Servers.update({name: serverName}, {$set: {lastUsedPort: port}}, function() {
-					Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port, password: password}});
-				});
-			};
+					Servers.update({name: serverName}, {$set: {lastUsedPort: port}}, function() {
+						Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port, password: password}});
+					});
+				};
+			});
 		});
 
 		Servers.add = function(name, ip, login, password, path, country, city) {
@@ -55,11 +56,39 @@ Meteor.startup(function() {
 			var sshConnection = new ssh2();
 
 			sshConnection.on('error', function(err) {
-				throw new Meteor.Error('Ошибка соединения с сервером');
-				return false;
+				Servers.remove({name: name});
 			});
 			
 			sshConnection.on('ready', function() {
+				Servers[name].start = function(matchId, serverName, game, map, type, team1_id, team2_id) {
+					if (!(matchId && serverName && map && type && team1_id && team2_id)) throw new Meteor.Error('Нет аргументов');
+					if (!Servers.findOne({name: serverName})) throw new Meteor.Error('Сервер не найден');
+					var server = Servers.findOne({name: serverName}),
+						team1Name = Teams.findOne(team1_id).name,
+						team2Name = Teams.findOne(team2_id).name,
+						maxPlayers = parseInt(type[0], 10) + parseInt(type[2], 10) + 1 ,
+						port = server.lastUsedPort + 1 ,
+						password = randomstring = Math.random().toString(36).slice(-8),
+						path = server[game].config.path;
+					switch (game) {
+						case 'cs16': {
+							Servers[server.name].sshConnection.exec('cd ' + path + ' && screen -AdmS comeback.cw-' + matchId + ' ./hlds_run -game cstrike -port ' + port + ' +maxplayers ' + maxPlayers + ' +map ' + map + ' sv_password ' + password + ' -pingboost 3 -master -secure', function() {
+								var rconConnection = new RCON(server.ip, port, server.password);
+								rconConnection.query('cw_start ' + team1Name + ' ' + team2Name + ' ' + matchId);
+							});
+						}
+						case 'csgo': {
+							// Future...
+						}
+						case 'css': {
+							// Future...
+						}
+					}
+					Servers.update({name: serverName}, {$set: {lastUsedPort: port}}, function() {
+						Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port, password: password}});
+					});
+				};
+
 				Servers.insert({
 					name: name,
 					ip: ip,
@@ -72,7 +101,7 @@ Meteor.startup(function() {
 					lastUsedPort: 27015
 				});
 			});
-			
+
 			sshConnection.connect({
 				host: ip,
 				port: 22,
