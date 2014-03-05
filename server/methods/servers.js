b51911dba643860d9b10a717f4f44f153180adb5
Maxpain177 <Maxpain177@gmail.com>
2014-03-05 23:01:55 +0700

    0.6.5.4

--- server/methods/servers.js
+++ server/methods/servers.js
@@ -1,6 +1,6 @@
 Meteor.methods({
 	addServer: function(name, ip, login, password, path, country, city) {
-		if (!this.userId || Roles.userIsInRole(this.userId, ['admin'])) throw new Meteor.Error(403, 'Permission denied');
+		if (!this.userId || !Roles.userIsInRole(this.userId, ['admin'])) throw new Meteor.Error(403, 'Permission denied');
 		if (!(name && ip && login && password && path && country && city)) throw new Meteor.Error(400, 'Bad request');
 		if (Servers.find({name: name}) || Servers.find({ip: ip})) throw new Meteor.Error(400, 'Такой сервер уже существует');
 		Servers.add(name, ip, login, password, path, country, city);
