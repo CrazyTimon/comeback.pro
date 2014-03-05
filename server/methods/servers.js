9a40528946c9d7f6d3644e88f680c11877c07efe
Maxpain177 <Maxpain177@gmail.com>
2014-03-05 23:08:20 +0700

    0.6.5.6

--- server/methods/servers.js
+++ server/methods/servers.js
@@ -4,6 +4,6 @@ Meteor.methods({
 		if (!this.userId || !Roles.userIsInRole(this.userId, ['admin'])) throw new Meteor.Error(403, 'Permission denied');
 		if (!(name && ip && login && password && path && country && city)) throw new Meteor.Error(400, 'Bad request');
 		if (Servers.findOne({name: name}) || Servers.findOne({ip: ip})) throw new Meteor.Error(400, 'Такой сервер уже существует');
-		Servers.add(name, ip, login, password, path, country, city);
+		return Servers.add(name, ip, login, password, path, country, city);
 	}
 });
