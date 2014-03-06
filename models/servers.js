6667991c432d912d15e620c49bf83ec6e9ba165f
Maxpain177 <Maxpain177@gmail.com>
2014-03-06 20:37:56 +0700

    small fix

--- models/servers.js
+++ models/servers.js
@@ -73,55 +73,54 @@ Meteor.startup(function() {
 			if (res.result.level === 'authentication') throw new Meteor.Error('Ошибка авторизации');
 
 			if (res.result === true && res.error === null) {
-					Servers.insert({
-						name: name,
-						ip: ip,
-						login: login,
-						password: password,
-						config: {
-							$addToSet: {
-								games: {
-									cs16: {
-										path: path
-									}
+				Servers.insert({
+					name: name,
+					ip: ip,
+					login: login,
+					password: password,
+					config: {
+						$addToSet: {
+							games: {
+								cs16: {
+									path: path
 								}
 							}
-						},
-						location: {
-							country: country,
-							city: city
-						},
-						lastUsedPort: 27015
-					}, function() {
-						Servers[name].start = function(matchId, serverName, game, map, type, team1_id, team2_id) {
-							if (!(matchId && serverName && map && type && team1_id && team2_id)) throw new Meteor.Error('Нет аргументов');
-							if (!Servers.findOne({name: serverName})) throw new Meteor.Error('Сервер не найден');
-							var server = Servers.findOne({name: serverName}),
-								team1Name = Teams.findOne(team1_id).name,
-								team2Name = Teams.findOne(team2_id).name,
-								maxPlayers = parseInt(type[0], 10) + parseInt(type[2], 10) + 1 ,
-								port = server.lastUsedPort + 1 ,
-								password = randomstring = Math.random().toString(36).slice(-8),
-								path = server.config.games[game].path;
-							switch (game) {
-								case 'cs16': {
-									Servers[server.name].sshConnection.exec('cd ' + path + ' && screen -AdmS comeback.cw-' + matchId + ' ./hlds_run -game cstrike -port ' + port + ' +maxplayers ' + maxPlayers + ' +map ' + map + ' sv_password ' + password + ' -pingboost 3 -master -secure', function() {
-										var rconConnection = new RCON(server.ip, port, server.password);
-										rconConnection.query('cw_start ' + team1Name + ' ' + team2Name + ' ' + matchId);
-									});
-								}
-								case 'csgo': {
-									// Future...
-								}
-								case 'css': {
-									// Future...
-								}
+						}
+					},
+					location: {
+						country: country,
+						city: city
+					},
+					lastUsedPort: 27015
+				}, function() {
+					Servers[name].start = function(matchId, serverName, game, map, type, team1_id, team2_id) {
+						if (!(matchId && serverName && map && type && team1_id && team2_id)) throw new Meteor.Error('Нет аргументов');
+						if (!Servers.findOne({name: serverName})) throw new Meteor.Error('Сервер не найден');
+						var server = Servers.findOne({name: serverName}),
+							team1Name = Teams.findOne(team1_id).name,
+							team2Name = Teams.findOne(team2_id).name,
+							maxPlayers = parseInt(type[0], 10) + parseInt(type[2], 10) + 1 ,
+							port = server.lastUsedPort + 1 ,
+							password = randomstring = Math.random().toString(36).slice(-8),
+							path = server.config.games[game].path;
+						switch (game) {
+							case 'cs16': {
+								Servers[server.name].sshConnection.exec('cd ' + path + ' && screen -AdmS comeback.cw-' + matchId + ' ./hlds_run -game cstrike -port ' + port + ' +maxplayers ' + maxPlayers + ' +map ' + map + ' sv_password ' + password + ' -pingboost 3 -master -secure', function() {
+									var rconConnection = new RCON(server.ip, port, server.password);
+									rconConnection.query('cw_start ' + team1Name + ' ' + team2Name + ' ' + matchId);
+								});
 							}
-							Servers.update({name: serverName}, {$set: {lastUsedPort: port}}, function() {
-								Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port, password: password}});
-							});
-						};
-					});
+							case 'csgo': {
+								// Future...
+							}
+							case 'css': {
+								// Future...
+							}
+						}
+						Servers.update({name: serverName}, {$set: {lastUsedPort: port}}, function() {
+							Matches.update({_id: matchId}, {$set: {ip: server.ip, port: port, password: password}});
+						});
+					};
 				});
 			}
 		};
