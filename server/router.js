b9c285e67c8d459fb375070fb08b3125f1d075ff
Maxpain177 <Maxpain177@gmail.com>
2014-03-09 20:32:42 +0700

    0.7.1.1

--- server/router.js
+++ server/router.js
@@ -3,61 +3,58 @@ Router.map(function () {
 		where: 'server',
 		path: '/server/:key/sendInfo/:typeRequest/:request/',
 		action: function () {
-			if (!this.params.key) this.response.writeHead(403);
-			if (this.params.key === Comeback.api.privateKey) {
-				var Request = JSON.parse(this.params.request.replace(/'/g,'"'));
-				switch (this.params.typeRequest) {
-					case 'score':
-						Matches.update(Request.matchId, { $set: 
-							{
-								'team1.score': Request.score1,
-								'team2.score': Request.score2
-							}
-						});
-						break;
-					case 'event':
-						switch (Request.eventName) {
-							case 'serverOn':
-								Matches.update(Request.matchId, { 
-									$set: {
-										'gamestatus': 'warmup'
-									}
-								});
-								break;
-							case 'serverOff':
-								Matches.remove(Request.matchId);
-								break;
-							case 'knifeStarted':
-								Matches.update(Request.matchId, { 
-									$set: {
-										'gamestatus': 'knife'
-									}
-								});
-								break;
-							case 'half1Started':
-								Matches.update(Request.matchId, { 
-									$set: {
-										'gamestatus': 'half1'
-									}
-								});
-								break;
-							case 'half2Started':
-								Matches.update(Request.matchId, { 
-									$set: {
-										'gamestatus': 'half2'
-									}
-								});
-								break;
-							case 'overtimeStarted':
-								Matches.update(Request.matchId, { 
-									$set: {
-										'gamestatus': 'overtime'
-									}
-								});
-								break;
+			if (!this.params.key || (this.params.key !== Comeback.api.privateKey)) this.response.writeHead(403);
+			var Request = JSON.parse(this.params.request.replace(/'/g,'"'));
+			switch (this.params.typeRequest) {
+				case 'score':
+					Matches.update(Request.matchId, { $set: {
+							'team1.score': Request.score1,
+							'team2.score': Request.score2
 						}
-						break;
-				}
+					});
+					break;
+				case 'event':
+					switch (Request.eventName) {
+						case 'serverOn':
+							Matches.update(Request.matchId, { 
+								$set: {
+									'gamestatus': 'warmup'
+								}
+							});
+							break;
+						case 'serverOff':
+							Matches.remove(Request.matchId);
+							break;
+						case 'knifeStarted':
+							Matches.update(Request.matchId, { 
+								$set: {
+									'gamestatus': 'knife'
+								}
+							});
+							break;
+						case 'half1Started':
+							Matches.update(Request.matchId, { 
+								$set: {
+									'gamestatus': 'half1'
+								}
+							});
+							break;
+						case 'half2Started':
+							Matches.update(Request.matchId, { 
+								$set: {
+									'gamestatus': 'half2'
+								}
+							});
+							break;
+						case 'overtimeStarted':
+							Matches.update(Request.matchId, { 
+								$set: {
+									'gamestatus': 'overtime'
+								}
+							});
+							break;
+					}
+					break;
 			}
 		}
 	});
