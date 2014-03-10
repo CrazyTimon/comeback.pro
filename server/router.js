e6cb8f1218d62e3d2a999c13a19613bac19d2c79
Maxpain177 <Maxpain177@gmail.com>
2014-03-10 19:35:03 +0700

    1

--- server/router.js
+++ server/router.js
@@ -3,9 +3,9 @@ Router.map(function () {
 		where: 'server',
 		path: '/server/:key/sendInfo/:typeRequest/:request/',
 		action: function () {
-			console.log(this.params);
 			if (!this.params.key || (this.params.key !== Comeback.api.privateKey)) this.response.writeHead(403);
 			var Request = JSON.parse(this.params.request.replace(/'/g,'"'));
+			console.log(Request);
 			switch (this.params.typeRequest) {
 				case 'score':
 					Matches.update(Request.matchId, { $set: {
@@ -54,6 +54,13 @@ Router.map(function () {
 								}
 							});
 							break;
+						case 'gameEnd': {
+							Matches.update(Request.matchId, {
+								$set: {
+									'status': 'finished'
+								}
+							});
+						}
 					}
 					break;
 			}
